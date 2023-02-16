647fbbc5397af56136fed0fb6a5602b80728f076
marve <makomire9@gmail.com>
2023-02-16 10:13:00 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - A program that uses puts to print a string 
+ *Return: Always 0
+ */ 
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzel");
+return (0); 
+}
