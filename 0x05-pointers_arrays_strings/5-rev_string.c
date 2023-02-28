757f35bc1ab49b6cbebc5c234a63b7aa81c9c34a
marve <makomire9@gmail.com>
2023-02-27 20:12:57 -0800

    0x05;
    ;

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,3 +16,29 @@ for (i = 0; s[i]; i++)
 {
 length++;
 }
+
+count = length;
+
+start_c = s;
+end_c = s;
+
+
+for (i = 0; i < count - 1; i++)
+{
+end_c++;
+}
+
+
+for (i = 0; i < count / 2; i++)
+{
+
+
+c = *end_c;
+*end_c = *start_c;
+*start_c = c;
+
+
+start_c++;
+end_c--;
+}
+}
